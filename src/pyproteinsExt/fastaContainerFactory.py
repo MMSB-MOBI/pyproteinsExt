4795f4e398ed4ef174eaa613fd0163ac2efb79e3
cecilpert <cecile.hilpert@gmail.com>
2019-05-23 12:10:36 +0200

    Add fasta container

---
+++ src/pyproteinsExt/fastaContainerFactory.py
@@ -0,0 +1,45 @@
+import pyproteinsExt.proteinContainer
+
+def parse(inputFile=None):
+    bigBuffer = ''
+    instances_list=[]
+    mainContainer = Container()
+    fnOpen = open
+    t = 'r'
+    if inputFile.endswith('gz'):  
+        fnOpen = gzip.open
+        t = 'rt'
+    try:
+        f = fnOpen(inputFile, t)
+    except IOError:
+        print ("Could not read file:", inputFile)
+        return Container()
+
+    instance=[]
+    with f:
+        for l in f:
+            if l.startswith(">") : 
+                if instance : 
+                    mainContainer=mainContainer.addParsing(Container(input=instance))        
+                instance=[]
+            instance.append(l.rstrip())    
+        mainContainer=mainContainer.addParsing(Container(input=instance))
+    return mainContainer
+
+
+class Container(pyproteinsExt.proteinContainer.Container):
+    def __init__(self,input=None):
+       super().__init__(_parseBuffer,input)
+
+def _parseBuffer(input):
+    header=input[0].lstrip(">")
+    prot=header.split(" ")[0]
+    seq="".join(input[1:])
+    obj=Fasta(prot,header,seq)
+    return {prot:obj}
+
+class Fasta():
+    def __init__(self,prot,header,seq):
+        self.prot=prot 
+        self.seq=seq
+        self.header=header     
--- src/pyproteinsExt/topology.py
+++ src/pyproteinsExt/topology.py
@@ -1,5 +1,6 @@
 import pyproteinsExt.hmmrContainerFactory as hmmr 
 import pyproteinsExt.tmhmmContainerFactory as tmhmm
+import pyproteinsExt.fastaContainerFactory as fasta
 import pyproteinsExt.proteinContainer
 
 def check_if_same_proteins(dic_container):
@@ -10,7 +11,9 @@ def check_if_same_proteins(dic_container):
     if dic_container['tmhmm']:    
         tmhmm_proteins=set([e.prot for e in dic_container['tmhmm']])
         list_proteins.append(tmhmm_proteins)
-    
+    if dic_container['fasta']:
+        fasta_proteins=set([e.prot for e in dic_container['fasta']])
+        list_proteins.append(fasta_proteins)  
     if len(list_proteins)<=1: 
         return True
     for i in range(len(list_proteins)):
@@ -24,13 +27,13 @@ def check_if_same_proteins(dic_container):
     return True            
 
 
-
-def parse(hmmrOut,tmhmmOut):
+def parse(hmmrOut,tmhmmOut,fastaOut):
     container=Container()
     hmmrContainer=hmmr.parse(hmmrOut)
-    tmhmmContainer=tmhmm.parse(tmhmmOut)    
+    tmhmmContainer=tmhmm.parse(tmhmmOut)
+    fastaContainer=fasta.parse(fastaOut)    
 
-    dic_container={'hmmr':hmmrContainer,'tmhmm':tmhmmContainer}
+    dic_container={'hmmr':hmmrContainer,'tmhmm':tmhmmContainer,'fasta':fastaContainer}
     if not check_if_same_proteins(dic_container):
         raise Exception("not same proteins ",hmmrOut,tmhmmOut,"Check !")
     container.addParsing(Container(input=dic_container))   
@@ -41,19 +44,22 @@ class Container(pyproteinsExt.proteinContainer.Container):
         super().__init__(_parseBuffer,input)
 
 class Topology(): 
-    def __init__(self,prot,hmmr,tmhmm):
+    def __init__(self,prot,hmmr,tmhmm,fasta):
         self.prot=prot
         self.hmmr=hmmr
         self.tmhmm=tmhmm
+        self.fasta=fasta
 
 def _parseBuffer(dic_container):
     hmmrContainer=dic_container['hmmr']
     tmhmmContainer=dic_container['tmhmm']
+    fastaContainer=dic_container['fasta']
     dic_obj={}
     for p in hmmrContainer.pIndex: 
         hmmr=hmmrContainer.pIndex[p]
         tmhmm=tmhmmContainer.entries[p]
-        obj=Topology(p,hmmrContainer.pIndex[p],tmhmm)
+        fasta=fastaContainer.entries[p]
+        obj=Topology(p,hmmrContainer.pIndex[p],tmhmm,fasta)
         dic_obj[p]=obj
     return dic_obj                   
 
