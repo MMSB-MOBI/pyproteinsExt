c957d28870e80a55fe0530f51856022f79d6cd4c
cecilpert <cecile.hilpert@gmail.com>
2019-05-23 11:49:46 +0200

    Add tmhmm parsing to Topology

--- src/pyproteinsExt/topology.py
+++ src/pyproteinsExt/topology.py
@@ -2,12 +2,12 @@ import pyproteinsExt.hmmrContainerFactory as hmmr
 import pyproteinsExt.proteinContainer
 
 def parse(hmmrOut=None):
+def parse(hmmrOut,tmhmmOut):
     container=Container()
-    hmmrContainer=None
-    if hmmrOut: 
-        hmmrContainer=hmmr.parse(hmmrOut)
+    hmmrContainer=hmmr.parse(hmmrOut)
+    tmhmmContainer=tmhmm.parse(tmhmmOut)    
 
-    dic_container={'hmmr':hmmrContainer}
+    dic_container={'hmmr':hmmrContainer,'tmhmm':tmhmmContainer}
     container.addParsing(Container(input=dic_container))   
     return container
 
@@ -16,15 +16,19 @@ class Container(pyproteinsExt.proteinContainer.Container):
         super().__init__(_parseBuffer,input)
 
 class Topology(): 
-    def __init__(self,prot,hmmr):
+    def __init__(self,prot,hmmr,tmhmm):
         self.prot=prot
         self.hmmr=hmmr
+        self.tmhmm=tmhmm
 
 def _parseBuffer(dic_container):
     hmmrContainer=dic_container['hmmr']
+    tmhmmContainer=dic_container['tmhmm']
     dic_obj={}
     for p in hmmrContainer.pIndex: 
-        obj=Topology(p,hmmrContainer.pIndex[p])
+        hmmr=hmmrContainer.pIndex[p]
+        tmhmm=tmhmmContainer.entries[p]
+        obj=Topology(p,hmmrContainer.pIndex[p],tmhmm)
         dic_obj[p]=obj
     return dic_obj                   
 
